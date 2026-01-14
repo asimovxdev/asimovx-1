6533c44449916fca5a22af5b66c04cc56e7c60df
DonBinoy <donbinoy25@gmail.com>
2026-01-15 01:40:02 +0530

    image fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Forced restart
-  reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
